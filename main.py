e0fa24369458f1301ff18256e423d7c13a6e0405
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2023-02-13 18:39:21 -0600

    Task 1.4

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+
 # Cargamos los datos desde el archivo .txt
 data = []
 with open("cleaned_entrenamiento.txt", "r") as f:
@@ -13,8 +19,8 @@ train = data[:split]
 test = data[split:]
 
 # Contamos el número de mensajes ham y spam
-n_ham_messages = len([x for x in train if x[0] == 'ham'])
-n_spam_messages = len([x for x in train if x[0] == 'spam'])
+n_ham_messages = len([x for x in train if x[0] == "ham"])
+n_spam_messages = len([x for x in train if x[0] == "spam"])
 total_messages = n_ham_messages + n_spam_messages
 
 # Contamos la frecuencia de cada palabra en los mensajes ham y spam
@@ -25,13 +31,13 @@ for label, message in train:
         if word in words:
             words[word][label] += 1
         else:
-            words[word] = {'ham': 0, 'spam': 0}
+            words[word] = {"ham": 0, "spam": 0}
             words[word][label] = 1
 
 # Calculamos la probabilidad de cada palabra en pertenecer a ham o spam
 for word in words:
-    words[word]['ham'] = (words[word]['ham'] + 1) / (n_ham_messages + 2)
-    words[word]['spam'] = (words[word]['spam'] + 1) / (n_spam_messages + 2)
+    words[word]["ham"] = (words[word]["ham"] + 1) / (n_ham_messages + 2)
+    words[word]["spam"] = (words[word]["spam"] + 1) / (n_spam_messages + 2)
 
 # Predecimos la clase de los mensajes en el subset de testing
 predictions = []
@@ -41,17 +47,17 @@ for label, message in test:
     message = message.split(" ")
     for word in message:
         if word in words:
-            prob_ham_message *= words[word]['ham']
-            prob_spam_message *= words[word]['spam']
+            prob_ham_message *= words[word]["ham"]
+            prob_spam_message *= words[word]["spam"]
         else:
             prob_ham_message *= 1 / (n_ham_messages + 2)
             prob_spam_message *= 1 / (n_spam_messages + 2)
 
-# Comparamos las probabilidades y clasificamos el mensaje como spam o ham
+    # Comparamos las probabilidades y clasificamos el mensaje como spam o ham
     if prob_spam_message > prob_ham_message:
-        predictions.append('spam')
+        predictions.append("spam")
     else:
-        predictions.append('ham')
+        predictions.append("ham")
 
 # Evaluamos el desempeño del modelo en el subset de testing
 correct_predictions = 0
@@ -70,16 +76,16 @@ for label, message in train:
     message = message.split(" ")
     for word in message:
         if word in words:
-            prob_ham_message *= words[word]['ham']
-            prob_spam_message *= words[word]['spam']
+            prob_ham_message *= words[word]["ham"]
+            prob_spam_message *= words[word]["spam"]
         else:
             prob_ham_message *= 1 / (n_ham_messages + 2)
             prob_spam_message *= 1 / (n_spam_messages + 2)
 
     if prob_spam_message > prob_ham_message:
-        train_predictions.append('spam')
+        train_predictions.append("spam")
     else:
-        train_predictions.append('ham')
+        train_predictions.append("ham")
 
 train_correct_predictions = 0
 for i in range(len(train)):
@@ -97,24 +103,56 @@ def classify_message(message, words, n_ham_messages, n_spam_messages):
     message = message.split(" ")
     for word in message:
         if word in words:
-            prob_ham_message *= words[word]['ham']
-            prob_spam_message *= words[word]['spam']
+            prob_ham_message *= words[word]["ham"]
+            prob_spam_message *= words[word]["spam"]
         else:
             prob_ham_message *= 1 / (n_ham_messages + 2)
             prob_spam_message *= 1 / (n_spam_messages + 2)
     if prob_spam_message > prob_ham_message:
-        return 'spam', prob_spam_message / (prob_spam_message + prob_ham_message)
+        return "spam", prob_spam_message / (prob_spam_message + prob_ham_message)
     else:
-        return 'ham', prob_ham_message / (prob_spam_message + prob_ham_message)
+        return "ham", prob_ham_message / (prob_spam_message + prob_ham_message)
 
 
 # Recibimos mensajes nuevos y los clasificamos
 while True:
     message = input("\nIngrese un mensaje para definir si es spam o ham: ")
-    label, prob = classify_message(
-        message, words, n_ham_messages, n_spam_messages)
+    label, prob = classify_message(message, words, n_ham_messages, n_spam_messages)
     print("\nEl mensaje es clasificado como: ", label)
     print("La probabilidad de ser", label, "es de: ", prob)
     continue_input = input("\n¿Desea ingresar otro mensaje? (Sí/No)").lower()
     if continue_input == "no":
         break
+
+# Task 1.4
+# Cargamos los datos del archivo .txt en un dataframe
+data = []
+with open("cleaned_entrenamiento.txt", "r") as f:
+    for line in f:
+        parts = line.strip().split("\t")
+        if len(parts) == 2:
+            label, message = parts
+            data.append([label, message])
+df = pd.DataFrame(data, columns=["label", "message"])
+
+# Dividimos los datos en un conjunto de entrenamiento y un conjunto de prueba
+train, test = train_test_split(df, test_size=0.2)
+
+# Convertimos los mensajes en una representación numérica con CountVectorizer
+vectorizer = CountVectorizer()
+train_features = vectorizer.fit_transform(train["message"])
+test_features = vectorizer.transform(test["message"])
+
+# Entrenamos un modelo de Bayes multinomial con MultinomialNB
+clf = MultinomialNB()
+clf.fit(train_features, train["label"])
+
+# Evaluamos la precisión del modelo en el conjunto de prueba
+train_predictions = clf.predict(train_features)
+train_accuracy = (train_predictions == train["label"]).mean()
+print("Accuracy en el subset de entrenamiento:", train_accuracy)
+
+# Evaluamos la precisión del modelo en el conjunto de prueba
+test_predictions = clf.predict(test_features)
+test_accuracy = (test_predictions == test["label"]).mean()
+print("Accuracy en el subset de prueba:", test_accuracy)
